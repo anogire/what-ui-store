33ba7bb0472f84551f0e7c697a520cc4d61c8cb6
Tatiana Ubozhenko <tatiana.medvedeva.100@gmail.com>
2021-05-21 11:50:22 +0300

    fix: telegram-bot model

--- app/telegram-bot/telegram-bot.model.js
+++ app/telegram-bot/telegram-bot.model.js
@@ -4,8 +4,7 @@ export default class ModelTelegramBot {
 
   async sendOrder(data) {
     const textFormat = this.getMessageView(data);
-    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?
-      chat_id=${CHAT_ID}&parse_mode=html&text=${textFormat}`;
+    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=html&text=${textFormat}`;
 
     try {
       await fetch(url);
