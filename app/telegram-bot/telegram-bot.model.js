add69c27f0011bad7353f17bee82dd2664cdb1d3
Tatiana Ubozhenko <tatiana.medvedeva.100@gmail.com>
2021-05-21 11:46:30 +0300

    fix: telegram-bot model

--- app/telegram-bot/telegram-bot.model.js
+++ app/telegram-bot/telegram-bot.model.js
@@ -3,8 +3,18 @@ import { TELEGRAM_BOT_TOKEN, CHAT_ID } from '../consts.js';
 export default class ModelTelegramBot {
 
   async sendOrder(data) {
-    const { customer: { name, phone, email }, order: { totalSum } } = data;
+    const textFormat = this.getMessageView(data);
+    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?
+      chat_id=${CHAT_ID}&parse_mode=html&text=${textFormat}`;
 
+    try {
+      await fetch(url);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getMessageView({ customer: { name, phone, email }, order: { totalSum } }) {
     const text = `
     <b>New order</b>!!
       <i>Customer:</i>!
@@ -13,13 +23,6 @@ export default class ModelTelegramBot {
         email:  ${email}!!
       <i>TotalSum:</i> ${totalSum}`;
 
-    const textFormat = text.replace(/[!\s]/g, mark => (mark == '!') ? '%0A' : '+');
-
-    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&parse_mode=html&text=${textFormat}`;
-    try {
-      await fetch(url);
-    } catch (err) {
-      console.log(err);
-    }
+    return text.replace(/[!\s]/g, mark => (mark == '!') ? '%0A' : '+');
   }
 }
